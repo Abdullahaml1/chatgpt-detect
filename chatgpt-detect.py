6ca8b47249eee83f8169208d5e7b11f28c3f2e0c
Abdullahaml1 <abdullah_aml@protonmail.com>
2023-06-11 02:48:00 +0300

    setting device

--- chatgpt-detect.py
+++ chatgpt-detect.py
@@ -216,7 +216,6 @@ def train(args, model, train_data_loader, dev_data_loader, accuracy, device):
 
         #### Your code here
         ans_tokens.to(device)
-        print('device', ans_tokens['input_ids'].device)
         labels.to(device)
 
 
