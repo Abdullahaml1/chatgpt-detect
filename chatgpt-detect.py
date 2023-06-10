7d07330f9a1c8227d7aa81814e6257003e4d24cc
Abdullahaml1 <abdullah_aml@protonmail.com>
2023-06-11 02:41:33 +0300

    setting device

--- chatgpt-detect.py
+++ chatgpt-detect.py
@@ -223,6 +223,7 @@ def train(args, model, train_data_loader, dev_data_loader, accuracy, device):
         pred = model(**ans_tokens)[0]
 
         # computing loss
+        pred.to(device)
         loss = criterion(pred, labels)
 
 
